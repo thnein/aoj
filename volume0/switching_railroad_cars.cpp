84f530f8c4f53300de163ce7df9309c7ac2867db
thnein <rheoineneetn@gmail.com>
2018-01-31 19:47:28 +0900

    Solution 0013

---
+++ volume0/switching_railroad_cars.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(void)
+{
+    vector<int> t;
+    int a;
+    while (cin >> a)
+    {
+        if (a != 0)
+        {
+            t.push_back(a);
+        }
+        else if (a == 0)
+        {
+            cout << t.back() << endl;
+            t.pop_back();
+        }
+    }
+}
